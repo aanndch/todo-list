5d988b9dafc94f1dce33a55ca549c6fc3890e3eb
Anand Chembarpu <anandchembarpu@gmail.com>
2019-05-10 18:32:35 +0530

    Add item done

---
+++ src/components/TodoList.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+import TodoForm from "./TodoForm";
+
+export default class TodoList extends Component {
+  state = {
+    todos: []
+  };
+
+  addTodo = todo => {
+    this.setState({
+      todos: [todo, ...this.state.todos]
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <TodoForm onSubmit={this.addTodo} />
+        {this.state.todos.map(todo => {
+          return (
+            <div>
+              {todo.id}
+              {todo.text}
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
+}
